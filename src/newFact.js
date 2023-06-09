b541af23a217cd6ff4e982505bb04f62aa684a17
Jeaneth Sanchez Nuñez <jeaneth.sanchez@gmail.com>
2023-06-09 13:46:02 +0200

    pruebas test

---
+++ src/newFact.js
@@ -0,0 +1,15 @@
+'use strict';
+
+export async function newFact() {
+    try {
+      const response = await fetch('https://uselessfacts.jsph.pl/api/v2/facts/random');
+      const data = await response.json();
+      
+      document.getElementById('curious-fact').textContent = data.text;
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  }
+
+  
+
