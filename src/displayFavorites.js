b21fece9c82cfaafb0b09e23ebe1f5336fa1cb79
Jeaneth Sanchez Nuñez <jeaneth.sanchez@gmail.com>
2023-06-12 15:07:26 +0200

    delete comments

--- src/displayFavorites.js
+++ src/displayFavorites.js
@@ -1,14 +1,14 @@
 window.addEventListener("DOMContentLoaded", displayFavorites);
 
 function displayFavorites() {
-  // Obtener el array de favoritos del almacenamiento local
+ 
   const favorites = localStorage.getItem("favorites");
   
   if (favorites) {
     const favoritesContainer = document.getElementById("favorites-container");
     const favoritesArray = JSON.parse(favorites);
     
-    // Crear un párrafo para cada favorito y agregarlo al contenedor
+   
     favoritesArray.forEach(favorite => {
       const paragraph = document.createElement("p");
       paragraph.textContent = favorite;
@@ -17,8 +17,3 @@ function displayFavorites() {
   }
 }
 
-window.addEventListener("beforeunload", clearFavorites);
-
-function clearFavorites() {
-localStorage.removeItem("favorites");
-}
